aecde61f290f3be11b876b401dcff74352afc13d
Raquel Martiniano <raquelmartiniano14@gmail.com>
2026-07-19 01:52:33 +0000

    começando o select

---
+++ atividade03/scripts/comandos_select.sql
@@ -0,0 +1,26 @@
+SELECT *
+    FROM Pagamento_pix;
+
+
+SELECT *
+    FROM Pagamento_cartao;
+
+
+SELECT *
+    FROM Pagamento_linha_credito;
+
+
+SELECT *
+    FROM Valor;
+
+
+SELECT *
+    FROM DescricaoProduto;
+
+
+SELECT *
+    FROM Pagamento_boleto;
+    
+
+SELECT *
+    FROM Pagamento;
